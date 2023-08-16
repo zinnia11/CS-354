817ad36709a8669b469f5e3bedb5ef7b4b2df222
zinnia11 <99695160+zinnia11@users.noreply.github.com>
2023-08-15 22:26:27 -0500

    Add files via upload

---
+++ Project 4/moap_functions.c
@@ -0,0 +1,26 @@
+
+// for each of the following functions 
+// correct the prototype
+// write function body
+// replace return with the correct return statement
+//
+// you may write as many helper functions here as you wish
+
+int algorithm_1() {
+    return 1;  
+}
+
+int algorithm_2() {
+    return 2;
+}
+
+int algorithm_3() {
+    return 3;
+}
+
+int algorithm_4() {
+    return 4;
+}
+
+
+
